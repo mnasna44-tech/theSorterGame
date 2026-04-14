723fbee72a9357dd7f93d91194f2318cfb51a0ad
mnasna44-tech <mnasna44@gmail.com>
2026-04-14 04:40:47 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,39 @@
-const CACHE_NAME = "game-cache-v1";
+const CACHE_NAME = "sorter-game-v1";
 
-self.addEventListener("install", function(event) {
+const FILES_TO_CACHE = [
+  "./",
+  "index.html",
+  "manifest.json"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll([
-        "index.html",
-        "manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response ? response : fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
